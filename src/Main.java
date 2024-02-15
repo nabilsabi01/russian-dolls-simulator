21e93fb14f5fbd210ba7250a547bf93504955d26
Nabil Sabi <nabilsabi01@gmail.com>
2024-02-15 17:07:52 +0100

    créer le menu de l'application

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,43 @@
+import enna.dev.RussianDoll;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Russian-doll-collection-management");
+        Scanner sc = new Scanner(System.in);
+
+        // insert the size of doll
+        RussianDoll p1 = new RussianDoll(70);
+        RussianDoll p2 = new RussianDoll(30);
+
+        int choice;
+        do {
+            System.out.println("taille de Poupée 1 est " + p1.getSize());
+            System.out.println("taille de Poupée 2 est " + p2.getSize());
+
+            System.out.println("|===========================================================================|");
+            System.out.println("|=============              Gestion Poupée Russe              ==============|");
+            System.out.println("|===========================================================================|");
+            System.out.println("|-------------  1: Placement une poupée dans une autre poupée   ------------|");
+            System.out.println("|-------------  2: Sort une poupée dans une autre poupée        ------------|");
+            System.out.println("|-------------  3: Quitter application                          ------------|");
+            System.out.println("|===========================================================================|");
+            System.out.println("Enter votre choix: ");
+            choice = sc.nextInt();
+            switch (choice){
+                case 1:
+                    System.out.println("Entrer le numero de première poupée : ");
+                    int pr1 = sc.nextInt();
+                    break;
+            }
+        }while (choice != 3);
+//        System.out.println("Enter la taille de poupée russe : ");
+//        int sd1 = sc.nextInt();
+//        System.out.println("Enter la taille de poupée russe : ");
+//        int sd2 = sc.nextInt();
+//        System.out.println("Enter la taille de poupée russe : ");
+//        int sd3 = sc.nextInt();
+
+
     }
 }
--- src/enna/dev/RussianDoll.java
+++ src/enna/dev/RussianDoll.java
@@ -2,59 +2,76 @@ package enna.dev;
 
 public class RussianDoll extends Doll{
     // fields
-    private Doll contained;
+    private Doll doll;
 
     // constructor parameter
     public RussianDoll(int size) {
         super(size);
-        contained = null;
+        doll = null;
     }
 
     // getter and setter
-    public Doll getContained() {
-        return contained;
+    public Doll getDoll() {
+        return doll;
     }
 
-    public void setContained(Doll contained) {
-        this.contained = contained;
+    public void setDoll(Doll doll) {
+        this.doll = doll;
     }
 
     // Override Methode
     // methode open the doll
     @Override
     public void open() {
-        if (!isOpened()){
-            this.setOpened(true);
+        if (isOpened()) {
+            System.out.println("La poupée est déjà ouverte.");
+        } else {
+            System.out.println("Ouverture de la poupée russe de taille " + getSize());
+            setOpened(true);
         }
     }
 
     // methode close the doll
     @Override
     public void close() {
-        if (isOpened()){
-            this.setOpened(false);
+        if (!isOpened()) {
+            System.out.println("La poupée est déjà fermée.");
+        } else if (doll != null) {
+            System.out.println("Veuillez d'abord sortir la poupée contenue avant de fermer.");
+        } else {
+            System.out.println("Fermeture de la poupée russe de taille " + getSize());
+            setOpened(false);
         }
     }
 
     // methode place the current doll in the p doll.
     @Override
     public void placeIn(Doll p) {
-        if (p.isOpened() && p.getSize() > this.getSize()){
-            this.contained = p;
-            p.close();
-        }else {
-            System.out.println("Impossible de placer la poupée dans " + p);
+        if (p.getSize() <= getSize()) {
+            System.out.println("La poupée ne peut pas être placée dans une poupée de taille inférieure ou égale.");
+        } else if (!p.isOpened()) {
+            System.out.println("Veuillez d'abord ouvrir la poupée de destination.");
+        } else {
+            System.out.println("Placement de la poupée russe de taille " + getSize() + " dans une poupée de taille " + p.getSize());
+            doll = (RussianDoll) p;
         }
     }
 
     // methode takes out the current doll from the p doll
     @Override
     public void outIn(Doll p) {
-        if (p.equals(this.contained)){
-            this.contained = null;
-            p.open();
-        }else {
-            System.out.println("La poupée n'est pas contenue dans " + p);
+        if (p != doll) {
+            System.out.println("La poupée n'est pas contenue dans cette poupée.");
+        } else {
+            System.out.println("Sortie de la poupée russe de taille " + getSize() + " de la poupée de taille " + p.getSize());
+            doll = null;
         }
+    }
+
+    @Override
+    public String toString() {
+        return "RussianDoll{" +
+                "doll=" + doll +
+                '}';
     }
 }
